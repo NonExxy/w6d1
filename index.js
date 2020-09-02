34219610ab48523b7301c5ea81c570f1b09d80b5
NonExxy <68622478+NonExxy@users.noreply.github.com>
2020-09-01 18:42:29 -0600

    Assignment Finalization

---
+++ index.js
@@ -0,0 +1,81 @@
+// Inventory array
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Phone',
+        price: 700.00,
+        inStock: false,
+        details: '5G'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    {
+        name: 'Smart Watch',
+        price: 200.00,
+        inStock: false,
+        details: 'Counts your steps'
+    },
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USB type C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for hours'
+    },
+];
+// This function loads all in-stock items on-screen
+$(storeItems).each(function() {
+    if (this.inStock === true) {
+      $('#appendHere').append(`<div class="contentBlock"><p>$${this.price}</p><p>${this.name}</p><p>${this.details}</p></div>`);
+    };
+   });
+// This is the button click function to activate dark mode
+$("#button").click(function(){
+    $('body').toggleClass('darkMode');
+    $('.contentBlock').toggleClass('darkBox');
+    $('.line').toggleClass('darkLine');
+    });
